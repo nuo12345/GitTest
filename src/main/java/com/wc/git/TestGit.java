105080be85894bf9dbbf6c19bc96b912c62e17d9
nuo1234 <2637063100@qq.com>
2023-01-16 15:21:20 +0800

    master second commit

---
+++ src/main/java/com/wc/git/TestGit.java
@@ -0,0 +1,18 @@
+package com.wc.git;
+
+/**
+ * @Author: Wc
+ * @Description:
+ * @Date: Created in 15:14 2023/1/16
+ * @Modified By:
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("hello git5!");
+        System.out.println("hello git6!");
+    }
+}
